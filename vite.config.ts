d2d077171ed37ab4d8a3e2357ecf2df9c355896a
nousdynamics <nousdynamicslta@gmail.com>
2026-04-10 08:00:25 -0300

    Update Vercel configuration to change redirect and rewrite paths from "aparelhosauditivos/indaiatuba" to "audiometria". Adjust base path in Vite config accordingly for consistent routing and asset handling.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/aparelhosauditivos/indaiatuba/",
+  base: "/audiometria/",
   plugins: [react(), tailwindcss()],
 })
